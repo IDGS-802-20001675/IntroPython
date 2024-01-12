56e6c4e5e103a06237444a3ff8004cc178a5a78c
vanessir <vanesolis929@gmail.com>
2024-01-11 22:23:30 -0600

    VFS Fundamentos basicos tuplas 11 de enero

---
+++ tema1-Variables.py
@@ -0,0 +1,18 @@
+#nombre de la variable
+
+numero1= "123"
+numero2= "18"
+nombre= "German"
+correo= "german@gmail.com"
+
+print(numero1 + numero2)
+#Conversión forzada entre tipos de datos
+
+print(int(numero1)+ int(numero2))
+
+sueldo= "123.283"
+valorDecimal=float(sueldo)
+print(valorDecimal)
+
+edad=110
+print(len(str(edad)))
